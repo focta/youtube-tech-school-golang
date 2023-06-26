236631cd3856724b154aeca11f1db1f82e91197d
focta <focke.wulf7234@gmail.com>
2023-06-27 07:14:12 +0900

    アカウントの出金/入金に関する部分を実装

--- db/sqlc/main_test.go
+++ db/sqlc/main_test.go
@@ -9,20 +9,22 @@ import (
 	_ "github.com/lib/pq"
 )
 
-var testQueries *Queries
-
 const (
 	driver = "postgres"
 	dbSource = "postgresql://yout:youtpass@localhost:15434/simple_bank?sslmode=disable"
 )
 
+var testQueries *Queries
+var testDB *sql.DB
+
 func TestMain(m *testing.M){
-	conn, err := sql.Open(driver, dbSource)
+	var err error
+	testDB, err = sql.Open(driver, dbSource)
 	if err != nil {
 		log.Fatal("cannot connect to db", err)
 	}
 
-	testQueries = New(conn)
+	testQueries = New(testDB)
 
 	// m.Run() はテストが終了したかどうかを数値で返送してくれるので、その値をそのままos.Exitの終了値の指定に利用するというもの
 	os.Exit(m.Run())
